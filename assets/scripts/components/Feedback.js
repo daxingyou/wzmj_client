b4153583d85a9b85b5f86e6023d6f119e40af0ad
xiong <g.wang.x@gmail.com>
2017-07-09 22:36:46 +0800

    fix bug

--- assets/scripts/components/Feedback.js
+++ assets/scripts/components/Feedback.js
@@ -11,6 +11,17 @@ cc.Class({
 		this.initButtonHandler('body/btnClose', 'onBtnCloseClicked');
     },
 
+	reset: function() {
+		var body = this.node.getChildByName('body');
+		var edtContent = cc.find('content/edtContent', body).getComponent(cc.EditBox);
+		var edtQQ = cc.find('qq/edtQQ', body).getComponent(cc.EditBox);
+		var edtPhone = cc.find('phone/edtPhone', body).getComponent(cc.EditBox);
+
+		edtContent.string = '';
+		edtQQ.string = '';
+		edtPhone.string = '';
+    },
+
 	initButtonHandler: function(path, cb) {
         var btn = cc.find(path, this.node);
         cc.vv.utils.addClickEvent(btn, this.node, 'Feedback', cb);
@@ -28,23 +39,30 @@ cc.Class({
 		var errmsg = null;
 
 		if (content == '') {
-			errmsg = '';
+			errmsg = '请填写反馈内容';
 		} else if (qq == '') {
-			errmsg = '';
-		} else if (qq == '') {
-			errmsg = '';
+			errmsg = '请填写QQ号码，以便我们联系您!';
+		} else if (phone == '') {
+			errmsg = '请填写电话号码，以便我们联系您!';
 		}
 
 		if (errmsg) {
-			cc.vv.alert.show('');
+			cc.vv.alert.show(errmsg);
 			return;
 		}
 
-		
+		// TODO: send to server
+		this.close();
+		cc.vv.alert.show('感谢您的反馈，我们会尽快处理!');
     },
 
 	onBtnCloseClicked: function(event) {
+		this.close();
+    },
+
+	close: function() {
 		cc.vv.utils.showDialog(this.node, 'body', false);
+		this.reset();
     },
 });
 
